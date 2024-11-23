05e346f42c684c97f2ca95d6dcbabac3a03c0180
braxda <dylanbracken30@gmail.com>
2024-11-23 12:47:26 -0500

    added geocoding program for one team

--- geocode.py
+++ geocode.py
@@ -7,3 +7,40 @@ import time
 df = pd.read_csv('michigan_roster_2023.csv')
 
 geolocator = Nominatim(user_agent='cfb_roster_analysis')
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+
+def get_coordinates(row):
+    #check if hometown is populated
+    if pd.isna(row['hometown']):
+        return pd.Series([None, None])
+
+    # combine address columns into one string
+    if pd.notna(row['country']) and row['country'] != 'USA' and pd.isna(row['state']):
+        # catch international players without a 'state'
+        address = f"{row['hometown']}, {row['country']}"
+    else:
+        address = f"{row['hometown']}, {row['state']}, {row['country']}"
+
+    print(f"getting coordinates for {row['name']}")
+
+    try:
+        #run geolocator
+        location = geocode(address)
+        if location:
+            print(f"Found: {location.latitude}, {location.longitude}")
+            #put lat and long into a list
+            return pd.Series([location.latitude, location.longitude])
+        else:
+            # if it finds no match for the address
+            print(f"No coordinates found for {address}")
+            return pd.Series([None, None])
+
+    except Exception as e:
+        print(f"Error geocoding {address}: {e}")
+        return pd.Series([None, None])
+
+print("Processing players...")
+df[['latitude', 'longitude']] = df.apply(get_coordinates, axis=1)
+
+df.to_csv('michigan_geocode.csv', index=False)
+print("Finished")
